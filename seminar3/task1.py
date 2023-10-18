21363e04298693fa17f269b2d2a7e3ff27c3fe90
Altasses <kurasov2000@gmail.com>
2023-10-18 23:40:26 +0500

    task1

---
+++ seminar3/task1.py
@@ -0,0 +1,16 @@
+# Дан список чисел. Определите, сколько в нем
+# встречается различных чисел.
+# Input: [1, 1, 2, 0, -1, 3, 4, 4]
+# Output: 6
+
+data = [int(i) for i in input('введите случайный ряд чисел: ').split()]
+print(data)
+
+print(len(set(data))) #set - множество
+
+d = {} # то же самое но через словарь
+for i in data:
+    if i not in d:
+        d[i] = 0
+    d[i] += 1
+print(len(d.keys()))
